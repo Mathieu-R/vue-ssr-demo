91153d60b4ffc8f5ca9296fa2d2a26528902db92
Mathieu-R <mathieu0709@gmail.com>
2017-07-27 12:30:26 +0200

    ssr

--- front/index.js
+++ front/index.js
@@ -1,7 +1,15 @@
 import Vue from 'vue';
 import App from './components/app';
 
-new Vue({
-  el: '#content',
-  render: h => h(App)
-});
+// universal entry
+// https://ssr.vuejs.org/en/structure.html
+
+export function createApp(ctx) {
+  const app = new Vue({
+    render: h => h(App)
+  });
+
+  return {app};
+}
+
+
